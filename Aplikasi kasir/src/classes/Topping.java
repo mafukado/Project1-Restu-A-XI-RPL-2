796bfb4a12fce1ae984db25b5258243f7bf598e6
mafukado <restuaufacahyadin02@gmail.com>
2021-03-06 21:23:39 +0700

    Implementasi Struktur kelas aplikasi kasir

---
+++ Aplikasi kasir/src/classes/Topping.java
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package classes;
+
+/**
+ *
+ * @author Meh
+ */
+public class Topping extends Menu {
+    
+    public Topping(String nama_topping, double harga) { }
+    
+}
