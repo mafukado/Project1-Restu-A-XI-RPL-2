bf3a10a78dbbbc41416e192a070a7365e598ab23
mafukado <restuaufacahyadin02@gmail.com>
2021-03-07 13:57:14 +0700

    Implementasi Kelas Daftar Menu

--- Aplikasi kasir/src/classes/DaftarMenu.java
+++ Aplikasi kasir/src/classes/DaftarMenu.java
@@ -7,10 +7,29 @@ package classes;
 import java.util.ArrayList;
 
 public class DaftarMenu {
-       private ArrayList<Menu> daftarMenu;
+       private final ArrayList<Menu> daftarMenu;
        
-       public DaftarMenu() { }
-       public void tambahMenu(Menu menu) { }
-       public void getMenuByKategori (String kategori) { }
-       public void tampilDaftarMenu() { }
+       public DaftarMenu() {
+           daftarMenu = new ArrayList<>();
+       }
+       public void tambahMenu(Menu menu) {
+           daftarMenu.add(menu);
+       }
+       public void getMenuByKategori (String kategori) {
+           System.out.println("======" + kategori +"======");
+           
+           for (int i = 0; i < daftarMenu.size(); i++) {
+               Menu m = daftarMenu.get(i);
+               if (m.getKategori() .equals(kategori)) {
+                   System.out.println((i + 1) + ". " + m.getNama_menu() + "\t" + m.getHarga());
+               }
+           }
+       }
+       public void tampilDaftarMenu() {
+           System.out.println("====== SELERORAMEN ======");
+           getMenuByKategori("Ramen");
+           getMenuByKategori("Kuah");
+           getMenuByKategori("Topping");
+           getMenuByKategori("Minuman");
+       }
 }
